673b66dd35e4be71c65ac8d02295c525865d426b
vonhuvinh149 <vonhuvinh149@gmail.com>
2022-08-20 11:32:54 +0700

    ss2

---
+++ ss5_module4/exercise/task1/product_management/src/main/java/com/repository/IProductRepository.java
@@ -0,0 +1,18 @@
+package com.repository;
+
+import com.model.Product;
+
+import java.util.List;
+
+public interface IProductRepository {
+    List<Product> findAll(String name);
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+
+}
---
+++ ss5_module4/exercise/task1/product_management/src/main/java/com/service/impl/ProductService.java
@@ -0,0 +1,40 @@
+package com.service.impl;
+
+import com.model.Product;
+import com.repository.IProductRepository;
+import com.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService implements IProductService {
+    @Autowired
+    private IProductRepository iProductRepository;
+
+    @Override
+    public List<Product> findAll(String name) {
+        return iProductRepository.findAll(name);
+    }
+
+    @Override
+    public void save(Product product) {
+        iProductRepository.save(product);
+    }
+
+    @Override
+    public Product findById(int id) {
+        return iProductRepository.findById(id);
+    }
+
+    @Override
+    public void update(int id, Product product) {
+        iProductRepository.update(id, product);
+    }
+
+    @Override
+    public void remove(int id) {
+        iProductRepository.remove(id);
+    }
+}
